84bd4e73862fad7b80d76d8e1dd44fb7ee5327ee
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2023-11-12 06:09:23 +0700

    Evox ccache

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/ProjectBlaze-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls ProjectBlaze-*.zip)"
-      rclone copy ~/rom/out/target/product/citrus/ProjectBlaze-*.zip 298:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls ProjectBlaze-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/citrus/evolution_citrus*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls evolution_citrus.zip)"
+      rclone copy ~/rom/out/target/product/citrus/evolution_citrus*.zip 298:citrus -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls evolution_citrus*.zip) Uploaded Successfully!"
 fi
