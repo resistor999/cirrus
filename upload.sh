0cc7b4160386d20a5b94650e06b2e1b99ccf66d5
lightnest57 <lightnest57@gmail.com>
2023-12-02 02:19:59 +0000

    dotos

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/Nusantara_v*.zip ]; then
+if [ -f ~/rom/out/target/product/citrus/dotOS-*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls evolution_citrus.zip)"
-      rclone copy ~/rom/out/target/product/citrus/Nusantara_v*.zip fk09:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls Nusantara_v*.zip) Uploaded Successfully!"
+      rclone copy ~/rom/out/target/product/citrus/dotOS-*.zip fk09:citrus -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls dotOS-*.zip) Uploaded Successfully!"
 fi
