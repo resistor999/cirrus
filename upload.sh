aa9acb7e6b48edfb07354f7bbf8495f51dc177ec
resistor999 p <capasitor999@proton.me>
2024-03-29 23:03:30 +0000

    ccache

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/dotOS*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls dotOS*.zip)"
-      rclone copy ~/rom/out/target/product/citrus/dotOS-*.zip fk09:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls dotOS*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/juice/CAFExtended-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls CAFExtended-*.zip)"
+      rclone copy ~/rom/out/target/product/juice/CAFExtended-*.zip fk09:juice -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls CAFExtended-*.zip) Uploaded Successfully!"
 fi
