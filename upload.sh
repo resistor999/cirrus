1031d46fff007dfe2800c5be0de89ed8f60e1013
resistor999 p <capasitor999@proton.me>
2024-03-30 09:14:20 +0000

    gas

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/juice/CAFExtended-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls CAFExtended-*.zip)"
-      rclone copy ~/rom/out/target/product/juice/CAFExtended-*.zip fk09:juice -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls CAFExtended-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/chime/xdCLO_*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/chime/ && ls xdCLO_*.zip)"
+      rclone copy ~/rom/out/target/product/chime/xdCLO_*.zip fk09:chime -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/chime/ && ls xdCLO_*.zip) Uploaded Successfully!"
 fi
